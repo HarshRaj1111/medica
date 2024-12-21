25a33f0beb9534e9e466f4d264a6a78fce625dc4
Harsh Raj <87993233+HarshRaj1111@users.noreply.github.com>
2024-12-21 16:54:55 +0530

    Update index.tsx

--- src/components/sections/dashboard/analytics/balance/index.tsx
+++ src/components/sections/dashboard/analytics/balance/index.tsx
@@ -1,28 +1,31 @@
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import CountryMenu from './CountryMenu';
+/*import IconifyIcon from 'components/base/IconifyIcon';*/
 
-const Balance = () => {
+const Earnings = () => {
   return (
-    <Stack
-      component={Paper}
-      alignItems="center"
-      justifyContent="space-between"
-      p={2.5}
-      height={100}
-    >
+    <Stack component={Paper} p={2.5} alignItems="center" spacing={2.25} height={100}>
+      {/*<Stack
+        alignItems="center"
+        justifyContent="center"
+        height={56}
+        width={56}
+        bgcolor="info.main"
+        borderRadius="50%"
+      >
+        <IconifyIcon icon="ic:round-bar-chart" fontSize="h2.fontSize" color="primary.main" />
+      </Stack>*/}
       <div>
         <Typography variant="body2" color="text.disabled">
-          Your Balance
+          Age
         </Typography>
         <Typography mt={0.25} variant="h3">
-          $1,000
+          21
         </Typography>
       </div>
-      <CountryMenu />
     </Stack>
   );
 };
 
-export default Balance;
+export default Earnings;
